d103d105a28ec14db6e49a0211b33d457c99f949
Vegar <vegar@epost.no>
2026-03-17 13:29:25 +0100

    Add: Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCYT87ZMVZ"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KCYT87ZMVZ');
+          `
+        }} />
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   )
